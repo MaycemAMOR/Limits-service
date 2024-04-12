a0acdbc2992f0018f6f679054fb806e9b91d78f6
Maycem_AMOR <maycem.amor@gmail.com>
2024-04-12 22:34:46 +0200

    -Enhance limits service to get configuration from application properties.

--- src/main/java/com/mytechmicroservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/com/mytechmicroservices/limitsservice/LimitsConfigurationController.java
@@ -1,15 +1,19 @@
 package com.mytechmicroservices.limitsservice;
 
 import com.mytechmicroservices.limitsservice.bean.LimitsConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsConfigurationController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
-    public LimitsConfiguration retrieveLimitsFromConfigurations(){
-        return new LimitsConfiguration(1000, 1);
+    public LimitsConfiguration retrieveLimitsFromConfigurations() {
+        return new LimitsConfiguration(configuration.getMaximum(), configuration.getMinimum());
 
     }
 }
--- src/main/java/com/mytechmicroservices/limitsservice/bean/LimitsConfiguration.java
+++ src/main/java/com/mytechmicroservices/limitsservice/bean/LimitsConfiguration.java
@@ -1,23 +1,7 @@
 package com.mytechmicroservices.limitsservice.bean;
 
-public class LimitsConfiguration {
+public record LimitsConfiguration(int maximum, int minimum) {
 
-    private int maximum;
-    private int minimum;
-
-    protected LimitsConfiguration() {
-    }
-
-    public LimitsConfiguration(int maximum, int minimum) {
-        this.maximum = maximum;
-        this.minimum = minimum;
-    }
-
-    public int getMaximum() {
-        return maximum;
-    }
-
-    public int getMinimum() {
-        return minimum;
-    }
 }
+
+
